ba5a02c50bab3b79bc235e4b5db9678bbdd90232
k363m611 <k363m611@gmail.com>
2024-11-16 21:09:40 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,39 @@
 # --------------------------------------------
+# Imports at the top - PyShiny EXPRESS VERSION
+# --------------------------------------------
+
+# From shiny, import just reactive and render
+from shiny import reactive, render
+
+# From shiny.express, import just ui
+from shiny.express import ui
+
+# Imports from Python Standard Library to simulate live data
+import random
+from datetime import datetime
+
+# --------------------------------------------
+# Optional: Import font awesome icons as you like
+# --------------------------------------------
+
+from faicons import icon_svg
+
+# --------------------------------------------
+# FOR OPTIONAL LOCAL DEVELOPMENT
+# --------------------------------------------
+
+# Add all packages not in the Std Library
+# to requirements.txt ONLY when working locally:
+#
+# faicons
+# shiny
+# shinylive
+#
+# And install them into an active project virtual environment (usually in .venv)
+# --------------------------------------------
+
+
+# --------------------------------------------
 # SET UP THE REACTIVE CONTENT
 # --------------------------------------------
 
@@ -21,3 +56,51 @@ def reactive_calc_combined():
     latest_dictionary_entry = {"temp": temp, "timestamp": timestamp}
 
     return latest_dictionary_entry
+
+
+# --------------------------------------------
+# Define the Shiny UI Page layout - Page Options
+# --------------------------------------------
+
+# Set the page title and enable the full-width layout
+ui.page_opts(title="PyShiny Express: Live Data (Basic)", fillable=True)
+
+
+# ------------------------------------------------
+# Define the Shiny UI Page layout - Sidebar
+# ------------------------------------------------
+with ui.sidebar(open="open"):
+    ui.h2("Antarctic Explorer", class_="text-center")
+    ui.p(
+        "A demonstration of real-time temperature readings in Antarctica.",
+        class_="text-center",
+    )
+
+
+# ------------------------------------------------
+# Define the Shiny UI Page layout - Main Content
+# ------------------------------------------------
+
+# Main content area - Current temperature
+ui.h2("Current Temperature")
+
+@render.text
+def display_temp():
+    """Get the latest reading and return a temperature string"""
+    latest_dictionary_entry = reactive_calc_combined()
+    return f"{latest_dictionary_entry['temp']} C"
+
+# Optional Font Awesome icon (e.g., sun icon)
+icon_svg("sun")
+
+# Add a horizontal rule for styling
+ui.hr()
+
+# Main content area - Current Date and Time
+ui.h2("Current Date and Time")
+
+@render.text
+def display_time():
+    """Get the latest reading and return a timestamp string"""
+    latest_dictionary_entry = reactive_calc_combined()
+    return f"{latest_dictionary_entry['timestamp']}"
